786bfa00b21ad0ae0ff6af8440ca6b8e3d9e97bf
estanip <estani.pettigrew@gmail.com>
2023-07-16 22:23:43 -0300

    Add service to get distance between two points

--- src/services/google-maps.ts
+++ src/services/google-maps.ts
@@ -10,4 +10,14 @@ async function getLocationCoordinates(placeId: string) {
     }
 };
 
-export { getLocationCoordinates };
+async function getDistanceBetweenTwoPoints(pointA, pointB) {
+    try {
+        const { data } = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${pointA}&destinations=${pointB}&key=${process.env.GOOGLE_API_KEY}`);
+        if (data?.status === 'OK') return data?.rows[0]?.elements[0]?.distance?.value;
+        else return null;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export { getLocationCoordinates, getDistanceBetweenTwoPoints };
